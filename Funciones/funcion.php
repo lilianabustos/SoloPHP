5f555139f894572e21596f5f06a86f0a65313a33
liliana <slbustos6@misena.edu.co>
2015-06-04 11:35:29 -0500

    funcion.php

---
+++ Funciones/funcion.php
@@ -0,0 +1,15 @@
+<?php
+
+function mifuncion(){
+
+	echo "yo soy yo";
+	echo "<br>";
+	echo "tu eres tu";
+	echo "<br>";
+	echo "nosotros somos nosotros";
+}
+mifuncion();
+mifuncion();
+mifuncion();
+mifuncion();
+?>
