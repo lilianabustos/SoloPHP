626416613520fa09957bb4954ee2553521a6e1b4
liliana <slbustos6@misena.edu.co>
2015-06-04 11:36:13 -0500

    funcionparametros.php

---
+++ Funciones/funcionparametros.php
@@ -0,0 +1,23 @@
+<?php
+
+
+//con esta funcion preparo una tabla de multiplicar
+function miTabla($numero){
+ for($multiplicador =0; $multiplicador<=10;$multiplicador++){
+
+ 	echo $numero."x".$multiplicador."=".$numero*$multiplicador."<br>";
+
+ }
+
+}
+
+//con este for calculo las tablas de multiplicar
+for ($otronumero=0; $otronumero<=10;$otronumero++){
+  
+  echo "tabla del".$otronumero."<br>";
+  
+   miTabla($otronumero);
+}
+
+
+?>
