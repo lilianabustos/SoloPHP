2314e72edc4025c1a2c06672c76c112857b3b656
liliana <slbustos6@misena.edu.co>
2015-06-05 06:37:33 -0500

    asignacion.php

---
+++ Operadores/asignacion.php
@@ -0,0 +1,23 @@
+<?php
+
+$mivariable =7;
+
+echo $mivariable;
+echo "<br>";
+
+$mivariable +=5;
+echo $mivariable;
+echo "<br>";
+
+$mivariable -=5;
+echo $mivariable;
+echo "<br>";
+
+$mivariable *=5;
+echo $mivariable;
+echo "<br>";
+$mivariable /=5;
+echo $mivariable;
+echo "<br>";
+
+?>
